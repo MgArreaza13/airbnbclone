2f8cd6461bdc886b581e546c5f4381119ebd5df3
Miguel Arreaza <mg.arreaza.13@gmail.com>
2021-04-26 10:49:46 -0400

    feat: create home Page and add Banner Component and Search component to the banner component

---
+++ src/components/home/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Banner from "../banner";
+import { HomeContainer } from "./styles";
+
+
+function Home() {
+  return (
+    <HomeContainer>
+      <Banner />
+    </HomeContainer>
+  );
+}
+
+export default Home;
