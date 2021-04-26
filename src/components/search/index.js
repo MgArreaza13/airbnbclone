7cae00f9e511fdf8f7ccc872a3e6dcd9c5bfb031
Miguel Arreaza <mg.arreaza.13@gmail.com>
2021-04-26 10:49:07 -0400

    feat: add search component

---
+++ src/components/search/index.js
@@ -0,0 +1,40 @@
+import React, { useState } from "react";
+import { SearchContainer } from "./styles";
+import "react-date-range/dist/styles.css";
+import "react-date-range/dist/theme/default.css";
+import { DateRangePicker } from "react-date-range";
+import { People } from "@material-ui/icons";
+import { Button } from "@material-ui/core";
+
+function Search() {
+  const [startDate, setStartDate] = useState(new Date());
+  const [endDate, setEndDate] = useState(new Date());
+
+  const selectionRange = {
+    startDate: startDate,
+    endDate: endDate,
+    key: "selection",
+  };
+
+  const handleSelect = (ranges) => {
+    setStartDate(ranges.selection.startDate);
+    setEndDate(ranges.selection.setEndDate);
+  };
+
+  return (
+    <SearchContainer>
+      <DateRangePicker ranges={[selectionRange]} onChange={handleSelect} />
+
+      <h2>
+        Number of guests
+        <People />
+      </h2>
+
+      <input type="number" min={0} defaultValue={2} />
+
+      <Button>Search Airbnb</Button>
+    </SearchContainer>
+  );
+}
+
+export default Search;
