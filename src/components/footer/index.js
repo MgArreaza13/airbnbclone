30a87a18ffd24f2de4f9080b08f94d2408993615
Miguel Arreaza <mg.arreaza.13@gmail.com>
2021-04-26 11:26:15 -0400

    feat: add footer component

---
+++ src/components/footer/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { FooterContainer } from "./styles";
+
+function Footer() {
+  return (
+    <FooterContainer>
+      <p>© 2020 Airbnb clone! No rights reserved - mgarreaza13!</p>
+      <p>Privacy · Terms · Sitemap · Company Details</p>
+    </FooterContainer>
+  );
+}
+
+export default Footer;
